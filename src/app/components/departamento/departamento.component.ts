df712c122d97032611fc4634f4dff884e23eb79e
Nunoporing <bahamondesbp@gmail.com>
2020-11-28 01:42:53 -0300

    Something

--- src/app/components/departamento/departamento.component.ts
+++ src/app/components/departamento/departamento.component.ts
@@ -4,39 +4,48 @@ import { ReservasService } from 'src/app/services/reservas.service';
 import { Departamento } from '../departamentos/departamentos';
 import { DepartamentoService } from '../departamentos/departamentos.service';
 
-
 @Component({
   selector: 'app-departamento',
   templateUrl: './departamento.component.html',
   styleUrls: ['./departamento.component.css']
 })
-export class DepartamentoComponent implements OnInit {
 
+export class DepartamentoComponent implements OnInit {
   depa:Departamento;
   departamentoId: string;
 
-  constructor( private actRoute: ActivatedRoute,
-              private _departamentosService: DepartamentoService,private reservasService: ReservasService ) { 
-
+  constructor(private actRoute: ActivatedRoute,
+              private _departamentosService: DepartamentoService,
+              private reservasService: ReservasService ) { 
+    
     this.actRoute.params.subscribe( params => {
       this._departamentosService.getDepartamento( params['id'] ).subscribe(
         (departamentos) => {
-          this.departamentoId =  params['id'];
+          this.departamentoId = params['id'];
           this.depa = departamentos;
-
         }
       );
     })
+
+    this.depa = {
+      "idDepartmento": 1,
+      "nombre": "Postman - Test",
+      "direccion": "Postman 32",
+      "region": "Metropolitana",
+      "ciudad": "Santiago",
+      "precio": 26000,
+      "disponibilidad": false
+    }
   }
 
   ngOnInit(): void {
   }
 
   reservarDepa() {
-    var fechai = '2020/10/14'
-    var fechat = '2020/10/16'
-    var dias = '23'
-    var idPersona = '41'
+    var fechai = '2020/10/14';
+    var fechat = '2020/10/16';
+    var dias = '23';
+    var idPersona = '41';
     var idDepartamento = this.departamentoId;
 
     this.reservasService.Crear(fechai,fechat,idDepartamento,idPersona,dias).subscribe(
--- src/app/components/departamentos/departamentos.service.ts
+++ src/app/components/departamentos/departamentos.service.ts
@@ -6,21 +6,20 @@ import { Departamento } from './departamentos';
 @Injectable({
   providedIn: 'root'
 })
+
 export class DepartamentoService {
 
-  private urlEndPoint:string = 'http://40.117.177.9:8080/api/v1/departamento/';
-  //private urlEndPoint:string = 'http://localhost:8080/api/v1/departamento/';
+  //private urlEndPoint:string = 'http://40.117.177.9:8080/api/v1/departamento/';
+  private urlEndPoint:string = 'http://localhost:8080/api/v1/departamento/';
 
   constructor(private http: HttpClient) { }
 
-  getDepartamentos(): Observable<Departamento[]> { 
-    
+  getDepartamentos(): Observable<Departamento[]> {
     return this.http.get<Departamento[]>(this.urlEndPoint+'all');
   }
 
   getDepartamento( idx: string ): Observable<Departamento> {
     return this.http.get<Departamento>(this.urlEndPoint+idx);
   }
-
   
 }
